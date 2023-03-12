50e68c9bbde80b437a70dddf77bd934be27fbcb3
AyushGurg <ayshgrg@gmail.com>
2023-03-12 10:51:48 -0600

    each cell only takes one value and it alternates

--- magic.js
+++ magic.js
@@ -28,11 +28,17 @@ const square = (colIndex, rowIndex) => {
 
 //function
 function play(i, j) {
-  if (Gameboard.playerTurn == "x") {
-    Gameboard.board[i][j].querySelector("[data-x]").classList.add("active");
+  if (Gameboard.playerTurn == "x" && !Gameboard.board[i][j].taken) {
+    Gameboard.board[i][j].spot
+      .querySelector("[data-x]")
+      .classList.add("active");
+    Gameboard.board[i][j].taken = true;
     Gameboard.playerTurn = "o";
-  } else if (Gameboard.playerTurn == "o") {
-    Gameboard.board[i][j].querySelector("[data-o]").classList.add("active");
+  } else if (Gameboard.playerTurn == "o" && !Gameboard.board[i][j].taken) {
+    Gameboard.board[i][j].spot
+      .querySelector("[data-o]")
+      .classList.add("active");
+    Gameboard.board[i][j].taken = true;
     Gameboard.playerTurn = "x";
   }
 }
@@ -46,8 +52,9 @@ let playerO = player("playerO", "o");
 for (let col = 0; col < Gameboard.board.length; col++) {
   for (let row = 0; row < Gameboard.board.length; row++) {
     let div = document.querySelector(".row-" + row + ".col-" + col);
-    Gameboard.board[col][row] = div;
-    Gameboard.board[col][row].addEventListener("click", function () {
+    let spotTaken = false;
+    Gameboard.board[col][row] = { spot: div, taken: spotTaken };
+    Gameboard.board[col][row].spot.addEventListener("click", function () {
       play(col, row);
     });
   }
